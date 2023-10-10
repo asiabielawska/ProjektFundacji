a1987a9757451556a1e7c4e87944ed7add08d071
Asia.Bielawska <joanna.bielawska8@gmail.com>
2023-10-10 19:00:29 +0200

    Added navigation bar, footer to all page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,39 @@
-import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import img from "../assets/młynmain.jpeg";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div>Fundacja Młyn Zyzaków</div>
-      <div>
-        <Link to={"o-fundacji"}>O Fundacji</Link>
-      </div>
-      <div>
-        <Link to={"historia"}>Historia</Link>
-      </div>
-      <div>
-        <Link to={"oferta"}>Oferta</Link>
-      </div>
-      <div>
-        <Link to={"galeria"}>Galeria</Link>
-      </div>
-      <div>
-        <Link to={"kontakt"}>Kontakt</Link>
+      <div
+        style={{
+          backgroundImage: `url(\"${img}\")`,
+          height: "600px",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          opacity: 0.8,
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            paddingTop: 250,
+            paddingLeft: 100,
+          }}
+        >
+          <div style={{ fontSize: 40 }}>Fundacja Młyn Zyzaków</div>
+          <div style={{ fontSize: 20, marginTop: 25, marginBottom: 25 }}>
+            Fundacja zlokalizowana w starym Młynie w Żywcu z unikatową historią.
+          </div>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "gray", textTransform: "none" }}
+            onClick={() => navigate("/o-fundacji")}
+          >
+            Dowiedz się więcej
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,77 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import img from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 type Props = {
   children: React.ReactNode;
 };
 
-export const Layout = ({ children }: Props) => {
+export function Layout({ children }: Props) {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>nawigacja</div>
+      <AppBar
+        component={"nav"}
+        style={{ background: "none", border: "none", boxShadow: "none" }}
+      >
+        <Toolbar>
+          <Typography sx={{ flexGrow: 1 }}>
+            <img
+              src={img}
+              width={"80px"}
+              alt="logo"
+              onClick={() => navigate("/")}
+              style={{
+                cursor: "pointer",
+              }}
+            />
+          </Typography>
+          <Link to={"/o-fundacji"} className="nav-link">
+            O Fundacji
+          </Link>
+          <Link to={"/historia"} className="nav-link">
+            Historia
+          </Link>
+          <Link to={"/oferta"} className="nav-link">
+            Oferta
+          </Link>
+          <Link to={"/galeria"} className="nav-link">
+            Galeria
+          </Link>
+          <Link to={"/kontakt"} className="nav-link">
+            Kontakt
+          </Link>
+        </Toolbar>
+      </AppBar>
+
       <div>{children}</div>
-      <div>footer</div>
+
+      <footer
+        style={{
+          backgroundColor: "#EAE0D5",
+          height: 100,
+          paddingLeft: 20,
+          position: "absolute",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          color: "#5E503F",
+        }}
+      >
+        <div style={{ fontSize: 15, paddingTop: 10 }}>
+          Fundacja Młyn Zyzaków
+        </div>
+        <div style={{ fontSize: 12, paddingTop: 5 }}>
+          Adres e-mail: FundacjaMlynZyzakow@gmail.com
+        </div>
+        <div style={{ fontSize: 12 }}>Telefon: 695 832 759</div>
+        <div style={{ fontSize: 15 }}>KRS: 0000944872</div>
+      </footer>
     </>
   );
-};
+}
