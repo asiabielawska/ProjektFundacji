1e391c39aa6d20732b119c8f5bf46411a9c8aa52
Asia.Bielawska <joanna.bielawska8@gmail.com>
2023-10-13 15:05:50 +0200

    Improve styling contact page and add text

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,26 @@
 import { MainHeading } from "../Layout/styled";
+import ImgOfMlyn from "../../assets/młynmain.jpeg";
+import {
+  ContactContainer,
+  ContactImg,
+  ContactAllText,
+  ContactElement,
+} from "./styled";
 
 export const Contact = () => {
-  return <MainHeading>Zapraszamy do kontaktu!</MainHeading>;
+  return (
+    <>
+      <MainHeading>Zapraszamy do kontaktu!</MainHeading>
+      <ContactContainer>
+        <ContactAllText>
+          <ContactElement>
+            Adres: ul. Sienkiewicza 89, 34-300 Żywiec
+          </ContactElement>
+          <ContactElement>e-mail: FundacjaMlynZyzakow@gmail.com</ContactElement>
+          <ContactElement>Telefon: 695 832 759</ContactElement>
+        </ContactAllText>
+        <ContactImg src={ImgOfMlyn}></ContactImg>
+      </ContactContainer>
+    </>
+  );
 };
